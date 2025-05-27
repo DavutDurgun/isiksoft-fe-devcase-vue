557777f30c2ad6afd7fc9061452d5816ebedf3f2
Davut <davut@Davut-MacBook-Pro.local>
2025-05-27 13:57:50 +0300

    feat: dumy pages and route add

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,5 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="p-4 bg-white shadow-md rounded-lg">
+    <h2 class="text-3xl font-bold text-center mb-6 text-gray-800">Uygulamaya Hoş Geldiniz!</h2>
+  </div>
 </template>
---
+++ src/views/ProductsView.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2 class="text-3xl font-bold text-center mb-6 text-gray-800">Tüm Ürünler</h2>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+/* ProductsView'e özel stiller */
+</style>
